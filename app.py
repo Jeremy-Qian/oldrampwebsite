5490903d99d3ccf517abc4a3d67e1bccde5002b5
Jeremy-Qian <917592095@qq.com>
2025-03-10 14:25:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.ballons()
+st.balloons()
 st.markdown("""#Domain Moved
 ##:material/mood_bad:
 Sorry. We've moved to a new domain. Visit it at [https://ramper.streamlit.app](https://ramper.streamlit.app). Thank you!
