946b678a2a5fcffb39a5d1ebddc49036e6a63be8
Jeremy-Qian <jeremytheking2013@gmail.com>
2025-05-30 18:43:16 +0800

    Update domain name

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.balloons()
 st.markdown("""# :rainbow[Domain Moved]  
 ## :material/mood_bad: 
-Sorry. We've moved to a new domain. Visit it at [https://ramper.streamlit.app](https://ramper.streamlit.app). Thank you!  
+Sorry. We've moved to a new domain. Visit it at [https://rampion.streamlit.app](https://rampion.streamlit.app). Thank you!  
 
 Jeremy Qian""")
